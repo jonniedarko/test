42b0a2c763388ad2d5332caace0df8960b60deda
John Maguire <jonnie.maguire@gmail.com>
2014-08-02 12:51:45 +0100

    Blah

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ app.controller('macroCtrl', function ($scope){
     $scope.user = { };
 
     $scope.goals = {
-	    current: {
+    	zero:{
+    		 title:"Select a Goal"
+	        ,calories: 0
+	        ,protein: 0
+	        ,fats: 0
+	        ,carbs:0
+    	}
+	    ,current: {
 	        title:"Mainteance"
 	        ,calories: 0
 	        ,protein: 0
@@ -25,80 +32,99 @@ app.controller('macroCtrl', function ($scope){
 	        ,carbs:0
 	    }
 	};
+	$scope.goal = $scope.goals.zero;
+	
 
+	//$scope.updateGoal()
+         
 	$scope.activityLevels = [{
-	        title: "Little or No Exercise"
-	        ,description: "You are Lazy and avoid exercise"
-	        ,value: 1.2
+	        title: "Fat Loss/Weight Loss"
+
+	        ,value: 1
 	    }
 	    ,{
-	        title: "Light Exercise"
-	        ,description: "Exercise 1-3 times a week"
-	        ,value: 1.375
+	        title: "Body weight maintenance "
+
+	        ,value: 2
 	    }
 	    ,{
-	        title: "Moderate Exercise"
-	        ,description: "Exercise 3-5 times a week"
-	        ,value: 1.55
-	    }
-	    ,{
-	        title: "Heavey Exercise"
-	        ,description: "You Exercise 6-7 times a week"
-	        ,value: 1.725
-	    }
-	    ,{
-	        title: "Professional Athlete"
-	        ,description: "You Exercise approx 2 times a Day"
-	        ,value: 1.9
+	        title: "Lean muscle gain"
+
+	        ,value: 3
 	    }
 	];
 
 
     $scope.getMetaRate = function(){
 
-		var meta; 
-		if($scope.user.sex === "male"){
-			meta = (88.362 + (13.397 * parseFloat(  $scope.user.weight) ) 
-					+(5.799 * parseFloat(  $scope.user.height) )
-					-(5.677 * parseFloat(  $scope.user.age) ) || 0 ); 
-					
-		}else if($scope.user.sex === "female"){
-			meta = (447.593 + (9.247 * parseFloat(  $scope.user.weight) )
-					+(3.098 * parseFloat(  $scope.user.height) )
-					-(4.33 * parseFloat(  $scope.user.age) ) || 0 );
-		}
-		else{ meta = null;}
+		
 
-		$scope.user.bmr= meta;
-
-		return (!isNaN(meta) && (meta!=null)) ? parseFloat(meta).toFixed(2) : "Please Fill Out the above Form";
+		return 1;//(!isNaN(meta) && (meta!=null)) ? parseFloat(meta).toFixed(2) : "Please Fill Out the above Form";
 
 	};
 	$scope.updateNutrition=function(){
-	    if(($scope.user.sex === "male" || $scope.user.sex === "female") && $scope.user.weight>0 && $scope.user.height>0 && $scope.user.age>0 && $scope.user.activityLevel>0){
-	        $scope.goals.current.calories = parseFloat($scope.user.bmr * $scope.user.activityLevel)|| 0;
-	        $scope.goals.loss.calories = $scope.goals.current.calories * 0.85 || 0;
-	        $scope.goals.gain.calories = $scope.goals.current.calories * 1.15 || 0;
+		//console.log('updateNutrition() : activityLevel', $scope.user.activityLevel );
+		//console.log('updateNutrition() : weight', $scope.user.weight );
+		//console.log('updateNutrition() : sex', $scope.user.sex );
 
-	        $scope.goals.current.protein = $scope.user.weight*2.204 || 0;
-	        $scope.goals.loss.protein = $scope.user.weight*2.204 || 0;
-	        $scope.goals.gain.protein = $scope.user.weight*2.204*1.5 || 0;
+		if($scope.user.weight !== 0 
+			&& $scope.user.weight !== undefined 
+			&& $scope.user.activityLevel !== "" 
+			&& $scope.user.activityLevel !== undefined ){
 
-	        $scope.goals.current.carbs = ($scope.goals.current.calories*0.3)/4 || 0; 
-	        $scope.goals.loss.carbs = ($scope.goals.loss.calories*0.15)/4 || 0;
-	        $scope.goals.gain.carbs = ($scope.goals.gain.calories*0.3)/4 || 0;
+			console.log('updateNutrition() : BAM');
+			
+			if($scope.user.sex === "male"){
+				$scope.goal.normalcalories = $scope.user.weight *30;
+				$scope.goal.protein = $scope.user.weight *2.2;
 
-	        $scope.goals.current.fats = ($scope.goals.current.calories -($scope.goals.current.protein*4)-($scope.goals.current.carbs*4))/9 || 0;
-	        $scope.goals.loss.fats = ($scope.goals.loss.calories -($scope.goals.loss.protein*4)-($scope.goals.loss.carbs*4))/9 || 0;
-	        $scope.goals.gain.fats = ($scope.goals.gain.calories -($scope.goals.gain.protein*4)-($scope.goals.gain.carbs*4))/9 || 0;
+			}
+			else if($scope.user.sex === "female"){
+				$scope.goal.normalcalories = $scope.user.weight *30;
+				$scope.goal.protein = $scope.user.weight *2.2;
+				
+
+			}
+			$scope.updateGoal();
 	    }
+	    
+	};
+	$scope.updateGoal=function(){
+		console.log('$scope.goal.normalcalories',$scope.goal.normalcalories);
+		if(($scope.user.activityLevel === 1 || $scope.user.activityLevel === '1') &&($scope.user.sex ==='male' || $scope.user.sex ==='female')){
+
+			var mul = 1;
+			if($scope.user.sex ==='male'){
+				 mul = 0.8;
+			}else if($scope.user.sex ==='female'){
+				 mul = 0.75;
+			}
+
+			$scope.goal.calories = $scope.goal.normalcalories * mul;
+			$scope.goal.fats = $scope.goal.calories /30;
+			$scope.goal.carbs = ($scope.goal.calories - ($scope.goal.protein  *4) - ($scope.goal.fats / 9))/4;
+			console.log('$scope.goal.fats',$scope.goal.fats);
+
+		}
+		else if($scope.user.activityLevel === 2 || $scope.user.activityLevel === '2'){
+			$scope.goal.calories = $scope.goal.normalcalories * 1;
+			$scope.goal.fats = $scope.goal.calories /30;
+			$scope.goal.carbs = ($scope.goal.calories - ($scope.goal.protein  *4) - ($scope.goal.fats / 9))/4;
+
+		}
+		else if($scope.user.activityLevel === 3 || $scope.user.activityLevel === '3'){
+			$scope.goal.calories = $scope.goal.normalcalories * 1.2;
+			$scope.goal.fats = $scope.goal.calories /30;
+			$scope.goal.carbs = ($scope.goal.calories - ($scope.goal.protein  *4) - ($scope.goal.fats / 9))/4;
+
+		}
 	}
 
 	$scope.getName = function(){
             if($scope.user.name===undefined || $scope.user.name==="")
-                return "";
+                return "What's your Goal?";
 
-            return $scope.user.name+"'s";
+            return 'What\'s your Goal '+$scope.user.name+'?';
         }
 
 });
